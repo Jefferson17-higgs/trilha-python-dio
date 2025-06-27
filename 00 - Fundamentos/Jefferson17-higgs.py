4792b66ef6d95f9a744207eefd2a0d210760522c
jeff <jeffersonfaculdade17@gmail.com>
2025-06-27 11:05:53 -0300

    Ajustes de otimização

--- 00 - Fundamentos/Jefferson17-higgs.py
+++ 00 - Fundamentos/Jefferson17-higgs.py
@@ -1,39 +1,38 @@
 # sistema bancario
 
-conta_bancaria= float(3000)
+conta_bancaria= 3000.00
 limite_diario_saques= 0
 lista_deposistos= []
 lista_saques= []
 
 MENU= 'Menu'
 EXTRATO= 'Extrato'
-
+extrato= ''
 while True:
     
     print('')
     print(MENU.center(24, '-'))
-    print('''\n
+    menu= '''\n
           
     1. Deposito
     2. Saque
     3. Extrato
     4. Sair
           
-          ''')
+    Opção =>  '''
+    escolha_usuario= int(input(menu))
     print('-'*24,'\n')
-
-    escolha_usuario= int(input('Selecione a opção que deseja: '))
 
     if escolha_usuario == 1:
         
         print(f'\nSaldo em conta: {conta_bancaria:.2f}')
         deposito_usuario= int(input('Digite o valor de deposito: '))
         
-        permitir_deposito= conta_bancaria+deposito_usuario  if deposito_usuario > 0 else 'Valor invalido, selecione putro valor.'
+        permitir_deposito= conta_bancaria+deposito_usuario  if deposito_usuario > 0 else 'Valor invalido, selecione outro valor.'
         conta_bancaria= permitir_deposito
-        lista_deposistos.append(deposito_usuario)    
-        print(f'Saldo atual: {conta_bancaria:.2f}' if permitir_deposito == type(1) else permitir_deposito)
-    
+        extrato+= f'Deposito: R$ {deposito_usuario:.2f}\n' 
+        print(f'Saldo atual: {conta_bancaria:.2f}' if type(permitir_deposito) != str else permitir_deposito)
+      
     elif escolha_usuario == 2:
         
             saque_usuario= int(input('\nDigite o valor de saque: '))
@@ -46,8 +45,8 @@ while True:
                 
                     permitir_saque= conta_bancaria-saque_usuario if conta_bancaria > 0 else 'Não a saudo suficiente'
                     conta_bancaria= permitir_saque
-                    lista_saques.append(saque_usuario)
-                    print(f'Saldo atual: {conta_bancaria:.2f}' if permitir_saque == type(1) else permitir_saque)
+                    extrato+= f'Saque: {saque_usuario:.2f}\n'
+                    print(f'Saldo atual: {conta_bancaria:.2f}' if type(permitir_saque) != str else permitir_saque)
                 
                 else:
                     
@@ -56,33 +55,15 @@ while True:
             else:
                 
                 print('Limite de R$ 500.00 excedido.')
-    
-            
+                
     elif escolha_usuario == 3:
-        
+                   
         print('')
-        print(EXTRATO.center(27, '-'))
-        
-        for deposito, saque in zip(lista_deposistos, lista_saques):
-            
-
-            print(f'''\n 
-                
-                Depositos: {deposito:.f}
-                Saques: {saque:.2f}
-                
-                
-                
-                ''')     
-                
-        else:
-            
-            print('Não foram realizadas movimentações.')
-              
-              
-        print(f'Saldo atual da conta: {conta_bancaria:.2f}\n')
+        print(EXTRATO.center(27, '-'),'\n')
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {conta_bancaria:.2f}")
         print('-'*24,'\n')
-        
+       
     elif escolha_usuario == 4:
         
         print('\nOperação encerrada, obrigado.')
